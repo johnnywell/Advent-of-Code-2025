9db740e2136dae1144f2c09d188f7a1138b1c00e
Johnny Wellington <johnny@arbeit.studio>
2025-12-09 09:03:37 -0300

    Day 2 part one solution optimizing by the possible invalid ids

--- day2/part_one.rb
+++ day2/part_one.rb
@@ -1,16 +1,27 @@
 sum = 0
-File.foreach("input.txt").with_index(1) do |id_ranges|
-	ranges = id_ranges.split(",")
-	ranges.each do |range|
-		start_num, end_num = range.split("-").map(&:to_i)
-		(start_num..end_num).each do |candidate_id|
-			candidate_id_str = candidate_id.to_s
-			length = candidate_id_str.length
-			first_half = candidate_id_str[0...length/2]
-			second_half = candidate_id_str[length/2..length]
-			sum += candidate_id if first_half == second_half
+ranges = []
+
+File.read("input.txt").strip.split(",").each do |range|
+	start_num, end_num = range.split("-").map(&:to_i)
+	ranges << [start_num, end_num]
+end
+
+# Gera todos os IDs inválidos possíveis
+(1..5).each do |pattern_length|  # o máximo são 10 dígitos, então pattern length 5
+	first_half_start = 10 ** (pattern_length - 1)
+	first_half_end = 10 ** pattern_length - 1
+	
+	(first_half_start..first_half_end).each do |first_half|
+		invalid_id = (first_half.to_s * 2).to_i
+		
+		# Checa se o invalid_id está em algum range que lemos do input
+		ranges.each do |start_num, end_num|
+			if invalid_id >= start_num && invalid_id <= end_num
+				sum += invalid_id
+				break # sai pra não contar duas vezes 
+			end
 		end
 	end
 end
 
-puts "Total #{sum}"
+puts "Total: #{sum}"
